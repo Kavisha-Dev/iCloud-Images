0babd89187426a711299c7bfe78fdf7850f55c45
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2013-12-16 11:15:29 -0500

    saving to Documents directory

--- CloudImageDemo/ViewController.m
+++ CloudImageDemo/ViewController.m
@@ -12,6 +12,7 @@
 
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 - (IBAction)uploadImage:(id)sender;
+- (IBAction)showImage:(id)sender;
 
 @end
 
@@ -38,18 +39,63 @@
     [self presentViewController:myPicker animated:YES completion:nil];
 }
 
+
 // an image is selected
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
     [self dismissViewControllerAnimated:YES completion:nil];
     UIImage *chosenImage = [info objectForKey:UIImagePickerControllerOriginalImage];
-    self.imageView.image = chosenImage;
+    // self.imageView.image = chosenImage;
+    
+    // instead of displaying the image, let's save it to our Documents directory
+    [self saveImageToDocuments:chosenImage];
 }
 
 // user hits cancel
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)showImage:(id)sender {
+    
+    // load image from documents and display it
+    self.imageView.image = [self loadFromDocuments];
+}
+
+
+#pragma mark - File Save methods
+
+- (void)saveImageToDocuments:(UIImage *)image {
+    
+    NSData *imageData = UIImageJPEGRepresentation(image, 1);
+    NSString *filePath = [self grabFilePath:@"testPicture"];
+    [imageData writeToFile:filePath atomically:YES];
+}
+
+- (UIImage *)loadFromDocuments {
+    
+    NSString *filePath = [self grabFilePath:@"testPicture"];
+    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:filePath];
+    UIImage *image = [[UIImage alloc]init];
+    
+    if (!fileExists) {
+        NSLog(@"File does not exist.");
+    } else {
+        // load image from Documents directory
+        NSData *imageData = [NSData dataWithContentsOfFile:filePath];
+        image = [UIImage imageWithData:imageData];
+    }
+    return image;
+}
+
+
+- (NSString *)grabFilePath:(NSString *)fileName {
+    
+    NSString *filePath = [[NSString alloc]initWithFormat:@"Documents/%@", fileName];
+    NSString *documentsFilePath = [NSHomeDirectory()stringByAppendingPathComponent:filePath];
+    
+    return documentsFilePath;
 }
 
 @end
